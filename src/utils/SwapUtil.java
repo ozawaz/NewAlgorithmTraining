ca68908166eefcc0793fa81300ae40680478ab75
ozawa <1261276386@qq.com>
2022-03-28 22:46:31 +0800

    一数为K次其他M

--- src/utils/SwapUtil.java
+++ src/utils/SwapUtil.java
@@ -9,8 +9,8 @@ package utils;
  */
 public class SwapUtil {
 
-    public static <E> void swap(E[] arr, int a, int b) {
-        E temp = arr[a];
+    public static <T> void swap(T[] arr, int a, int b) {
+        T temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
