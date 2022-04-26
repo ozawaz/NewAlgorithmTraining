fd5d08721bcd3b7d11b44e970e15855114a75f6b
ozawa <ozawaz@163.com>
2022-04-26 20:36:55 +0800

    最小生成树_Kruskal

--- src/code/graph/Edge.java
+++ src/code/graph/Edge.java
@@ -1,5 +1,7 @@
 package code.graph;
 
+import java.util.Objects;
+
 public class Edge {
 	public int weight;
 	public Node from;
@@ -9,5 +11,22 @@ public class Edge {
 		this.weight = weight;
 		this.from = from;
 		this.to = to;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Edge edge = (Edge) o;
+		return weight == edge.weight;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(weight);
 	}
 }
