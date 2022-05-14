7ef9a7395c5047b44c3aa9f378ae18a7b06fa3e3
ozawa <ozawaz@163.com>
2022-05-14 16:12:38 +0800

    货币组成金额同面值货币相同--动态规划

--- src/code/fortyToEighty/Code43_货币组成金额同面值货币相同.java
+++ src/code/fortyToEighty/Code43_货币组成金额同面值货币相同.java
@@ -1,5 +1,8 @@
 package code.fortyToEighty;
 
+import utils.CommonUtil;
+import utils.RandomUtil;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -59,5 +62,81 @@ public class Code43_货币组成金额同面值货币相同 {
             ways += process(coins, zhangs, index + 1, rest - (zhang * coins[index]));
         }
         return ways;
+    }
+
+    public static int dp1(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        Info info = getInfo(arr);
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+        int n = coins.length;
+        int[][] dp = new int[n + 1][aim + 1];
+        dp[n][0] = 1;
+        for (int index = n - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                int ways = 0;
+                for (int zhang = 0; zhang <= zhangs[index] && coins[index] * zhang <= rest; zhang++) {
+                    ways += dp[index + 1][rest - (zhang * coins[index])];
+                }
+                dp[index][rest] = ways;
+            }
+        }
+
+        return dp[0][aim];
+    }
+
+    public static int dp2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        Info info = getInfo(arr);
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+        int n = coins.length;
+        int[][] dp = new int[n + 1][aim + 1];
+        dp[n][0] = 1;
+        for (int index = n - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                // 首先累加下层的值
+                dp[index][rest] = dp[index + 1][rest];
+                // 判断左侧是否有值，有则累加
+                if (rest - coins[index] >= 0) {
+                    dp[index][rest] += dp[index][rest - coins[index]];
+                }
+                // 判断是否有重复结果
+                if (rest - coins[index] * (zhangs[index] + 1) >= 0) {
+                    // 减去下层的重复结果
+                    dp[index][rest] -= dp[index + 1][rest - coins[index] * (zhangs[index] + 1)];
+                }
+            }
+        }
+
+        return dp[0][aim];
+    }
+
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue = 20;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = RandomUtil.generateRandomPositiveArray(maxLen, maxValue);
+            int aim = (int) (Math.random() * maxValue);
+            int ans1 = coinsWay(arr, aim);
+            int ans2 = dp1(arr, aim);
+            int ans3 = dp2(arr, aim);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println("Oops!");
+                CommonUtil.printArray(arr);
+                System.out.println(aim);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                break;
+            }
+        }
+        System.out.println("测试结束");
     }
 }
